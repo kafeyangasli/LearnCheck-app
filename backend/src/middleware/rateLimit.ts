0fa95b94da5db34c704c8b3a9dc23b07cdf38160
Markus Prap Kurniawan <prapkurniawanmarkus@gmail.com>
2025-12-08 21:51:32 +0700

    adjust rate limit to 150 req/15min for production

--- backend/src/middleware/rateLimit.ts
+++ backend/src/middleware/rateLimit.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 500,
+  max: 150,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
